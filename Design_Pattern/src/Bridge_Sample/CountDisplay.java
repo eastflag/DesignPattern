853c48e4f3dca1158f29da8cfe7ba8a597ee2e0f
dongkee.lee <eastflag@lge.com>
2013-07-31 13:04:08 +0900

    Bridge update

--- Design_Pattern/src/Bridge_Sample/CountDisplay.java
+++ Design_Pattern/src/Bridge_Sample/CountDisplay.java
@@ -3,7 +3,7 @@ public class CountDisplay extends Display {
     public CountDisplay(DisplayImpl impl) {
         super(impl);
     }
-    public void multiDisplay(int times) {       // timesȸ �ݺ��ؼ� ǥ���ϱ�
+    public void multiDisplay(int times) {       // times회 반복해서 표시하기
         open();
         for (int i = 0; i < times; i++) {
             print();
