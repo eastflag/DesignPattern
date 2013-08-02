149878544525b2d92e68d8918e6b5b028713c73c
eastflag <eastflag@eastflag-PC>
2013-08-03 00:52:29 +0900

    decorator

---
+++ Design_Pattern/src/Decorator_Sample/Border.java
@@ -0,0 +1,7 @@
+package Decorator_Sample;
+public abstract class Border extends Display {
+    protected Display display;          
+    protected Border(Display display) { 
+        this.display = display;
+    }
+}
