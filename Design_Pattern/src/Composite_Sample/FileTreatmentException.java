ff8bf35f709486b0ce1915731b2a07e0e0266841
eastflag <eastflag@eastflag-PC>
2013-08-05 01:05:57 +0900

    composite

---
+++ Design_Pattern/src/Composite_Sample/FileTreatmentException.java
@@ -0,0 +1,8 @@
+package Composite_Sample;
+public class FileTreatmentException extends RuntimeException {      
+    public FileTreatmentException() {
+    }
+    public FileTreatmentException(String msg) {
+        super(msg);
+    }
+}
