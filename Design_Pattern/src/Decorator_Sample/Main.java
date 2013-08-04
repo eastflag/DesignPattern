6cd444e03680f69626a73d488e37e9bb6bac395a
eastflag <eastflag@eastflag-PC>
2013-08-04 14:26:51 +0900

    Strategy

--- Design_Pattern/src/Decorator_Sample/Main.java
+++ Design_Pattern/src/Decorator_Sample/Main.java
@@ -1,5 +1,10 @@
 package Decorator_Sample;
 public class Main {
+	//핵심기능과 부가기능을 분리
+	//핵심기능 : 스트링 표시
+	//부가기능 : 스트링 꾸미기
+	//핵심 인스턴스에 동적으로 부가기능을 추가한다.
+	//예: 슈퍼마리오가 아이템을 먹으면 레이저를 쏘고, 점프를 더 높이 하고 등.
     public static void main(String[] args) {
         Display b1 = new StringDisplay("Hello, world.");
         Display b2 = new SideBorder(b1, '#');
---
+++ Design_Pattern/src/Strategy_Sample/Strategy.java
@@ -0,0 +1,5 @@
+package Strategy_Sample;
+public interface Strategy {
+    public abstract Hand nextHand();
+    public abstract void study(boolean win);
+}
