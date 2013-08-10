e778141a193df168d95d13074eb70542c1e4880a
dongkee.lee <eastflag@lge.com>
2013-08-10 12:35:32 +0900

    Mediator pattern

---
+++ Design_Pattern/src/Mediator_Sample/Main.java
@@ -0,0 +1,9 @@
+package Mediator_Sample;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main {
+    static public void main(String args[]) {
+        new LoginFrame("Mediator Sample");
+    }
+}
---
+++ Design_Pattern/src/Mediator_Sample/Mediator.java
@@ -0,0 +1,5 @@
+package Mediator_Sample;
+public interface Mediator {
+    public abstract void createColleagues();
+    public abstract void colleagueChanged();
+}
